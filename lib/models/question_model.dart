97640afce4bc782c892f98e5ee8ee16cc7bf193f
unknown <damesidauruk2810@gmail.com>
2025-11-02 21:03:06 +0700

    feat(core): Implementasi model Question dan data dummy

--- lib/models/question_model.dart
+++ lib/models/question_model.dart
@@ -2,12 +2,12 @@ class Question {
   final String questionText;
   final List<String> options;
   final String correctAnswer;
-  final String explanation; // Tambahkan properti explanation
+  final String explanation;
 
   Question({
     required this.questionText,
     required this.options,
     required this.correctAnswer,
-    required this.explanation, // Tambahkan di constructor
+    required this.explanation,
   });
 }
